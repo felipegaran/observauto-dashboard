ff6c3d3bdb0244c62ba3bd8f098b4c52a463e691
felipegaran <48029626+felipegaran@users.noreply.github.com>
2025-09-22 23:59:51 -0500

    Update data.js

--- api/data.js
+++ api/data.js
@@ -3,7 +3,6 @@ import { promises as fs } from 'fs';
 import { fileURLToPath } from 'url';
 
 export default async function handler(request, response) {
-  // --- Cabeceras de CORS ---
   response.setHeader('Access-Control-Allow-Origin', '*');
   response.setHeader('Access-Control-Allow-Methods', 'GET');
   response.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -13,21 +12,11 @@ export default async function handler(request, response) {
   }
 
   try {
-    // 1. Encontrar la ruta del directorio actual de la función.
     const currentDir = path.dirname(fileURLToPath(import.meta.url));
-    
-    // 2. Construir la ruta al archivo data.json que está en la misma carpeta.
     const jsonFilePath = path.join(currentDir, 'data.json');
-
-    // 3. Leer el contenido del archivo.
     const fileContents = await fs.readFile(jsonFilePath, 'utf8');
-
-    // 4. Convertir el texto a un objeto JSON.
     const data = JSON.parse(fileContents);
-
-    // 5. Enviar los datos.
     response.status(200).json(data);
-
   } catch (error) {
     console.error(error);
     response.status(500).json({ 
